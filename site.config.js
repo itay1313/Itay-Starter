9f1d1b39f32c3fa3fd813ebd58740fe2fa1f785a
itay haephrati <itay1313@gmail.com>
2022-04-08 23:48:16 +0300

    header

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 export const siteConfig = {
   titleTemplate: 'Itay Haephrati', // Site default title. <titleTemplate> - <title>
-  title: 'your title there', // Default title attached to the template if none is provided.
-  description: 'your meta description there', // Website description used for meta description tag.
-  author: "Author's Name", // Author of the website.
-  twitter: '@author', // Twitter of the author.
+  title: 'My new website', // Default title attached to the template if none is provided.
+  description: 'Coding from the heart', // Website description used for meta description tag.
+  author: 'Itay Haephrati', // Author of the website.
+  twitter: '@itaycode', // Twitter of the author.
   lang: 'en',
   image: 'ogimage.jpg',
   url: 'https://www.staticstarter.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/sub-folder/.
-  themeColor: '#fff', // Used for setting manifest and progress theme colors.
+  themeColor: '#EBE8DA', // Used for setting manifest and progress theme colors.
   backgroundColor: '#fff', // Used for setting manifest background color.
   // any other variables worth adding
   // all the other variables that you will add there can be used throughout all the html files
